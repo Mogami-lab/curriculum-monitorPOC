51407cdf4033828517ea25970faa6662050a8852
Mogami-lab <amokale@gmail.com>
2026-06-01 22:48:10 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,5 @@
 
 window.__ENV = {
   SUPABASE_URL:      'https://bbeomlmrhiddwkmfqrtg.supabase.co'
-  SUPABASE_ANON_KEY: 'sb_publishable__BtE49ZzKRQN3YDmCW5bsw_MfBdROwC'
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJiZW9tbG1yaGlkZHdrbWZxcnRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4MjExMjMsImV4cCI6MjA5MDM5NzEyM30.WOj9m-F4v09_oEN1SozGE5-lqP-rZWOWsZynafGgkn8'
 };
